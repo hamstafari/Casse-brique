f3c64c17827b4606a32e07b1f85dd259a588f610
Stifeux <stifeu@hotmail.fr>
2018-02-05 11:39:47 +0100

    te

--- CasseBrique/src/Brique.java
+++ CasseBrique/src/Brique.java
@@ -1,4 +1,24 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Brique {
+import javax.swing.JComponent;
+
+public class Brique extends JComponent{
 	
+	public void Brique(Graphics g) {
+		int resistance = 0;
+		g.drawRect(10, 10, 10, 50);
+		int aleatoire =(int) (Math.random() * 3 );
+		switch(aleatoire) {
+		case 1:
+			g.setColor(Color.red);
+			resistance = 1;
+		case 2:
+			g.setColor(Color.green);
+			resistance = 2;
+		case 3:
+			g.setColor(Color.blue);
+			resistance = 3;
+		}
+	}
 }
--- CasseBrique/src/Interface.java
+++ CasseBrique/src/Interface.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Interface extends JFrame {
   public Interface(){                
     this.setTitle("Ma première fenêtre Java");
-    this.setSize(100, 150);
+    this.setSize(500, 500);
     this.setLocationRelativeTo(null);               
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
